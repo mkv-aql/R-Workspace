43d68a3e82e988e0a50a23d7af5c17d5fd7a89d3
mkv-aql <agammuhajir1@gmail.com>
2022-08-03 02:07:38 +0200

    First update

---
+++ 1-tidyverse-training/Part-1.R
@@ -0,0 +1,13 @@
+#Process of data import to visualization
+  #Using packages of 'dplyr' 'ggplot2'. Which are parts of 'tidyverse'
+
+## 1. Installing all packages
+  ## since 'install.package("")' doe not work, the packages were installed manually. 
+  ## 'http://cran.rstudio.com/bin/windows/contrib/4.2/' or 'https://cran.rstudio.com/src/contrib/'
+    ## Then, download the required packages "fs", "readxl","writexl","tidyverse","lubridate", "devtools"
+    ## Then installed using 'Tools > Install Packages'
+
+
+## 1.1 Installing packages SOLVED
+  ## use "dependencies = TRUE". Example: install.packages("tidyverse", dependencies = TRUE)
+
